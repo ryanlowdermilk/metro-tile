10ec8b96426361de5115580676dc0a6dfe471b8a
ryanlowdermilk <ryanlowdermilk@gmail.com>
2012-03-10 15:45:39 -0800

    init commit

---
+++ metro-tile.js
@@ -0,0 +1,43 @@
+$(function () {
+	$(".metro-tile").bind('mousedown', function (e) {
+		var touchLeft = 33;
+		var touchTop = 16;
+		var touchBottom = 72;
+		var touchRight = 66;
+		var x = e.pageX - this.offsetLeft - $(this).parent().offset().left;
+		var y = e.pageY - this.offsetTop - $(this).parent().offset().top;
+		var width = $(this).width();
+		var height = $(this).height();			
+		var wp = parseInt(x/width*100);
+		var hp = parseInt(y/height*100);
+		var left = (wp <= touchLeft && hp >= touchTop && hp < touchBottom);
+		var right = (wp >= touchRight && hp > touchTop && hp < touchBottom);
+		var top = (hp > 0 && hp <= touchTop);
+		var bottom = (hp > touchBottom);
+		var middle = (hp > touchTop && hp < touchBottom && wp > touchLeft && wp < touchRight);
+		if (right) $(this).addClass("tile_right");
+		else if (left) $(this).addClass("tile_left");
+		else if (bottom) $(this).addClass("tile_bottom");
+		else if (top) $(this).addClass("tile_top");
+		else if (middle) $(this).addClass("tile_middle");
+		console.log('x:' + x + ', y:' + y + ', h:' + height + ', w:' + width + ', wp' + wp + ', hp' + hp);
+		console.log('left:' + left);
+		console.log('right:' + right);
+		console.log('top:' + top);
+		console.log('bottom:' + bottom);
+		console.log('middle:' + middle);
+		return false;
+	});
+});
+
+$(function () {
+	$(".metro-surface").mouseup(function (x) {
+		$(".metro-tile").removeClass("tile_left");
+		$(".metro-tile").removeClass("tile_right");
+		$(".metro-tile").removeClass("tile_bottom");
+		$(".metro-tile").removeClass("tile_top");
+		$(".metro-tile").removeClass("tile_middle");
+		$(".metro-tile").addClass("tile_reset");
+		return false;
+	});
+});         
