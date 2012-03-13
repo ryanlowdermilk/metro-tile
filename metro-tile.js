28e533f057fd00059b4e9ef2adc00cb8af85f1fe
ryanlowdermilk <ryanlowdermilk@gmail.com>
2012-03-12 19:44:55 -0700

    fixed $ reference bug

--- metro-tile.js
+++ metro-tile.js
@@ -1,13 +1,13 @@
-$(function () {
-	$(".metro-tile").bind('mousedown', function (e) {
+jQuery(function () {
+	jQuery(".metro-tile").bind('mousedown', function (e) {
 		var touchLeft = 33;
 		var touchTop = 16;
 		var touchBottom = 72;
 		var touchRight = 66;
-		var x = e.pageX - this.offsetLeft - $(this).parent().offset().left;
-		var y = e.pageY - this.offsetTop - $(this).parent().offset().top;
-		var width = $(this).width();
-		var height = $(this).height();			
+		var x = e.pageX - this.offsetLeft - jQuery(this).parent().offset().left;
+		var y = e.pageY - this.offsetTop - jQuery(this).parent().offset().top;
+		var width = jQuery(this).width();
+		var height = jQuery(this).height();			
 		var wp = parseInt(x/width*100);
 		var hp = parseInt(y/height*100);
 		var left = (wp <= touchLeft && hp >= touchTop && hp < touchBottom);
@@ -15,23 +15,23 @@ $(function () {
 		var top = (hp > 0 && hp <= touchTop);
 		var bottom = (hp > touchBottom);
 		var middle = (hp > touchTop && hp < touchBottom && wp > touchLeft && wp < touchRight);
-		if (right) $(this).addClass("tile_right");
-		else if (left) $(this).addClass("tile_left");
-		else if (bottom) $(this).addClass("tile_bottom");
-		else if (top) $(this).addClass("tile_top");
-		else if (middle) $(this).addClass("tile_middle");
+		if (right) jQuery(this).addClass("tile_right");
+		else if (left) jQuery(this).addClass("tile_left");
+		else if (bottom) jQuery(this).addClass("tile_bottom");
+		else if (top) jQuery(this).addClass("tile_top");
+		else if (middle) jQuery(this).addClass("tile_middle");
 		return false;
 	});
 });
 
-$(function () {
-	$(".metro-surface").mouseup(function (x) {
-		$(".metro-tile").removeClass("tile_left");
-		$(".metro-tile").removeClass("tile_right");
-		$(".metro-tile").removeClass("tile_bottom");
-		$(".metro-tile").removeClass("tile_top");
-		$(".metro-tile").removeClass("tile_middle");
-		$(".metro-tile").addClass("tile_reset");
+jQuery(function () {
+	jQuery(".metro-surface").mouseup(function (x) {
+		jQuery(".metro-tile").removeClass("tile_left");
+		jQuery(".metro-tile").removeClass("tile_right");
+		jQuery(".metro-tile").removeClass("tile_bottom");
+		jQuery(".metro-tile").removeClass("tile_top");
+		jQuery(".metro-tile").removeClass("tile_middle");
+		jQuery(".metro-tile").addClass("tile_reset");
 		return false;
 });
 });
